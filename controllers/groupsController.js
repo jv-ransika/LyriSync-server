a5996160733def4b4c1b732fb40972d3b459c990
J.V.G. Ransika <jvgransika@gmail.com>
2024-05-18 10:58:18 +0530

    all work finw

---
+++ controllers/groupsController.js
@@ -0,0 +1,16 @@
+const {createHash} = require('crypto');
+
+
+class GroupController {
+    constructor(host) {
+        this.id = createHash('sha256').update(`${Date.now()}${host.ip}`).digest('base64').toString();
+        this.host = host;
+        this.members = [];
+    }
+
+    add_member(member) {
+        this.members.push(member);
+    }
+}
+
+module.exports = GroupController;
